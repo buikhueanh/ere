ee9e8358f26c77aa499695a2edac045ce34d6220
buikhueanh <anhbui_2026@depauw.edu>
2026-05-29 22:18:17 -0400

    add hero placeholder

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import Image from 'next/image';
+
 export default function HomePage() {
   return (
-    <main className="flex-1">
-      <h1 className="sr-only">ére</h1>
-    </main>
+    <>
+          {/* Hero */}
+      <section className="relative w-full h-screen flex items-center justify-center">
+        <Image
+          src="/images/hero/homepage.jpeg"
+          alt="ére"
+          width={1000}
+          height={800}
+          priority
+          className="object-cover"
+        />
+      </section>
+    </>
   );
 }
